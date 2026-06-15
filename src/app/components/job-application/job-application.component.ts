039cb855e8bc8d6cb6eca512f0bdd5b8bf0b09b0
pallavitiwade <tiwdepallavi97@gmail.com>
2026-06-15 15:02:59 +0530

    job application one

--- src/app/components/job-application/job-application.component.ts
+++ src/app/components/job-application/job-application.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-job-application',
   templateUrl: './job-application.component.html',
   styleUrls: ['./job-application.component.scss']
 })
-export class JobApplicationComponent implements OnInit {
+export class JobApplicationComponent {
+submit=false
 
-  constructor() { }
+Skills={
+  Html:false,
+  css:false,
+  Javascript:false,
+typescript:false
+}
+skillsubmit():boolean{
+  return Object.values(this.Skills).some(skill=>skill)
+}
 
-  ngOnInit(): void {
+title='job Application' 
+
+@ViewChild('job') job!:NgForm
+
+onsubmit(){
+  this.submit=true
+
+  if(this.job.invalid){
+    return this.job.control.markAllAsTouched()
   }
+  console.log(this.job.value);
+
+  if(this.skillsubmit()){
+    return
+  }
+  this.job.reset
+
+}
+
+
+  
 
 }
