992877530f6a2afe2ab1fbc60fa6ba630ee71f78
pallavitiwade <tiwdepallavi97@gmail.com>
2026-06-17 14:30:35 +0530

    change

---
+++ src/app/components/job-application-five/job-application-five.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-application-five',
+  templateUrl: './job-application-five.component.html',
+  styleUrls: ['./job-application-five.component.scss']
+})
+export class JobApplicationFiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
